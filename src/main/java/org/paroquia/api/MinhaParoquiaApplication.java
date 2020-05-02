58c00afb6c4e4ca9c98bcfd92609f57fb1fe2711
Jones Paulo Pozzatti <jones.pozzatti@gmail.com>
2020-05-01 21:19:29 -0300

    teste

--- src/main/java/org/paroquia/api/MinhaParoquiaApplication.java
+++ src/main/java/org/paroquia/api/MinhaParoquiaApplication.java
@@ -39,48 +39,48 @@ public class MinhaParoquiaApplication {
 			
 			
 		//	paroquiaService.testarServico();
-			Paroquia paroquia = new Paroquia();
-			paroquia.setRazaoSocial("Kazale IT");
-			paroquia.setCnpj("74645215000104");
-			paroquia.setEmail("paroquia@com.br");
-			Pastoral pastoral = new Pastoral();
-			
-			pastoral.setParoquia(paroquia);
-			pastoral.setDescricao("teste1");
-			pastoral.setEmail("paroquia@com.br");
-			pastoral.setNome("Batismo");
-			pastoral.setDataAtualizacao(new Date());
-						
-			Endereco endereco = new Endereco();
-			endereco.setCep("80320100");
-			endereco.setLogradouro("rua 1");
-			
-			paroquia.setEndereco(endereco);
-			
-			paroquia.getPastorais().add(pastoral);
-			
-			
-			Paroquia p = this.paroquiaRespository.save(paroquia);
-			
-			Pessoa usuario = new Pessoa();
-			usuario.setEmail("usuario@email.com");
-			usuario.setPerfil(PerfilEnum.ROLE_USUARIO);
-			usuario.setSenha(SenhaUtils.gerarBCrypt("123456"));
-			usuario.setNome("Joao");
-			usuario.setCpf("03138358968");
-			usuario.setDataNasc(new Date());
-			
-			this.pessoaRepository.save(usuario);
-			
-			Pessoa admin = new Pessoa();
-			admin.setEmail("admin@email.com");
-			admin.setPerfil(PerfilEnum.ROLE_ADMIN);
-			admin.setSenha(SenhaUtils.gerarBCrypt("123456"));
-			admin.setNome("Jones");
-			admin.setCpf("03138358969");
-			admin.setDataNasc(new Date());
-			
-			this.pessoaRepository.save(admin);
+//			Paroquia paroquia = new Paroquia();
+//			paroquia.setRazaoSocial("Kazale IT");
+//			paroquia.setCnpj("74645215000104");
+//			paroquia.setEmail("paroquia@com.br");
+//			Pastoral pastoral = new Pastoral();
+//			
+//			pastoral.setParoquia(paroquia);
+//			pastoral.setDescricao("teste1");
+//			pastoral.setEmail("paroquia@com.br");
+//			pastoral.setNome("Batismo");
+//			pastoral.setDataAtualizacao(new Date());
+//						
+//			Endereco endereco = new Endereco();
+//			endereco.setCep("80320100");
+//			endereco.setLogradouro("rua 1");
+//			
+//			paroquia.setEndereco(endereco);
+//			
+//			paroquia.getPastorais().add(pastoral);
+//			
+//			
+//			Paroquia p = this.paroquiaRespository.save(paroquia);
+//			
+//			Pessoa usuario = new Pessoa();
+//			usuario.setEmail("usuario@email.com");
+//			usuario.setPerfil(PerfilEnum.ROLE_USUARIO);
+//			usuario.setSenha(SenhaUtils.gerarBCrypt("123456"));
+//			usuario.setNome("Joao");
+//			usuario.setCpf("03138358968");
+//			usuario.setDataNasc(new Date());
+//			
+//			this.pessoaRepository.save(usuario);
+//			
+//			Pessoa admin = new Pessoa();
+//			admin.setEmail("admin@email.com");
+//			admin.setPerfil(PerfilEnum.ROLE_ADMIN);
+//			admin.setSenha(SenhaUtils.gerarBCrypt("123456"));
+//			admin.setNome("Jones");
+//			admin.setCpf("03138358969");
+//			admin.setDataNasc(new Date());
+//			
+//			this.pessoaRepository.save(admin);
 //
 //			List<Paroquia> paroquias = paroquiaRespository.findAll();
 //			paroquias.forEach(System.out::println);
